f8a8ee42407337a81aa3c55ee0faa43ce1156910
RAR <sswwqqaa@gmail.com>
2021-05-21 17:26:04 +0200

    refactoring and more fixes

--- pbil.py
+++ pbil.py
@@ -1,13 +1,19 @@
 import random
 
-population_size = 20
-max_weight = 26
+# Knapsack problem parameters
 items = [(12, 24), (7, 13), (11, 23), (8, 15), (9, 16)]
-n = len(items)  # number of all items
-best_knapsack = [0] * n  # create array filled with zeros of length n, best knapsack so far
+max_weight = 26
+# PBIL parameters
+population_size = 20
 generations = 20  # total amount of iterations
 mutation_probability = 0.2  # from 0 to 1
 mutation_value = 0.1  # from 0 to 1
+learning_rate1 = 0.1  # from 0 to 1
+learning_rate2 = 0.2  # from 0 to 1 and greater than learning_rate1
+
+n = len(items)  # number of all items
+best_knapsack = [0] * n  # create array filled with zeros of length n, best knapsack so far
+curr_generation = 1
 
 
 def value(item_set):
@@ -27,7 +33,7 @@ def weight(item_set):
 
 
 def create_population(probability_vec):
-    print("create_population")
+    print("create_population", end=" ")
     p = [[0] * n for _ in range(population_size)]  # empty 2d array
     for i in range(1, population_size):
         for j in range(0, n):
@@ -38,7 +44,7 @@ def create_population(probability_vec):
 
 
 def get_best_knapsack(population):
-    print("get_best_knapsack")
+    print("get_best_knapsack", end=" ")
     max_value = 0
     max_value_index = -1
     for i in range(1, population_size):
@@ -47,18 +53,14 @@ def get_best_knapsack(population):
             if curr_value > max_value:
                 max_value = curr_value
                 max_value_index = i
-    if max_value_index == -1:  # in case where there is no good candidate - try to fix it
-        return [0] * n  # empty array of length n
+    if max_value_index == -1:  # in case where there is no good candidate
+        return [0] * n
     return population[max_value_index]
 
 
 def update_prob_vector(knapsack, probability_vec):
-    print("update_prob_vector")
-    learning_rate1 = 0.1
-    learning_rate2 = 0.2  # greater than learning_rate1
+    print("update_prob_vector", end=" ")
     global best_knapsack
-    # print("x ", best_knapsack)
-    # print("xd ", knapsack)
     if value(best_knapsack) >= value(knapsack):
         alpha = learning_rate1
     else:
@@ -72,23 +74,23 @@ def update_prob_vector(knapsack, probability_vec):
 def mutate(probability_vec):
     print("mutate")
     for i in range(1, n):
-        r2 = random.uniform(0, 1)
-        if mutation_probability > random.uniform(0, 1):
-            probability_vec[i] = probability_vec[i] * (1 - mutation_value)
-            if r2 > 0.5 and probability_vec[i] + mutation_value <= 1:
-                probability_vec[i] = probability_vec[i] + mutation_value
+        if random.uniform(0, 1) < mutation_probability:
+            probability_vec[i] = probability_vec[i] * (1 - mutation_value) + random.uniform(0, 1) * mutation_value
     return probability_vec
 
 
 def pbil():
     knapsack = best_knapsack
     probability_vec = [.5] * n  # initial probability vector
+    global curr_generation
     for i in range(1, generations):
+        print(f"Processing {curr_generation:2} generation:", end=" ")
         population = create_population(probability_vec)
         knapsack = get_best_knapsack(population)
         probability_vec = update_prob_vector(knapsack, probability_vec)
         probability_vec = mutate(probability_vec)
-    print("Best knapsack: {}, Value: {}, Weight: {}".format(knapsack, value(knapsack), weight(knapsack)))
+        curr_generation += 1
+    print(f"Best knapsack: {knapsack}, Value: {value(knapsack)}, Weight: {weight(knapsack)}")
 
 
 if __name__ == '__main__':
