0383f3b087934f72cde9df5f3b16393abe24713a
RAR <sswwqqaa@gmail.com>
2021-05-21 23:14:36 +0200

    Added loading problems from files

--- pbil.py
+++ pbil.py
@@ -1,8 +1,5 @@
 import random
 
-# Knapsack problem parameters
-items = [(12, 24), (7, 13), (11, 23), (8, 15), (9, 16)]
-max_weight = 26
 # PBIL parameters
 population_size = 20
 generations = 100  # total amount of iterations
@@ -10,17 +7,32 @@ mutation_probability = 0.2  # from 0 to 1
 mutation_value = 0.1  # from 0 to 1
 learning_rate1 = 0.1  # from 0 to 1
 learning_rate2 = 0.2  # from 0 to 1 and greater than learning_rate1
-early_stopping_patience = 3  # int from 1
+early_stopping_patience = -1  # int from 1, -1 to skip
 
-n = len(items)  # number of all items
-best_knapsack = [0] * n  # create array filled with zeros of length n, best knapsack so far
+items = []
+max_weight = 0
+n = 0
+best_knapsack = []
+
+
+def read_problem_from_file(path):
+    global n, max_weight, best_knapsack
+    with open(path) as file:
+        n, max_weight = file.readline().split()
+        n = int(n)
+        max_weight = int(max_weight)
+        best_knapsack = [0] * n  # create array filled with zeros of length n, best knapsack so far
+        for i in range(1, n + 1):
+            row_items = file.readline().split()
+            items.append((int(row_items[0]), int(row_items[1])))
+    print(f"Loaded problem from {path}. n={n}, max_weight={max_weight}, items(value, weight)={items}")
 
 
 def value(item_set):
     curr_value = 0
     for i in range(1, n):
         if item_set[i] == 1:
-            curr_value += items[i][1]
+            curr_value += items[i][0]
     return curr_value
 
 
@@ -28,7 +40,7 @@ def weight(item_set):
     curr_weight = 0
     for i in range(1, n):
         if item_set[i] == 1:
-            curr_weight += items[i][0]
+            curr_weight += items[i][1]
     return curr_weight
 
 
@@ -83,6 +95,7 @@ def mutate(probability_vec):
 
 
 def pbil():
+    read_problem_from_file("problems/f1_l-d_kp_10_269")
     probability_vec = [.5] * n  # initial probability vector
     curr_generation = 1
     generation_value = []
@@ -96,21 +109,22 @@ def pbil():
 
         generation_value.append(value(knapsack))
         print(f"Current generation best knapsack: {knapsack}, Value: {value(knapsack)}, Weight {weight(knapsack)}")
-        early_stop = 0
-        if curr_generation > early_stopping_patience:
-            first_value = generation_value[-1 - early_stopping_patience]
-            print(f"Checking early stopping: {first_value}", end=" ")
-            j = -1
-            while j > -early_stopping_patience - 1:
-                print(f"{generation_value[j]}", end=" ")
-                if j == -early_stopping_patience:
-                    print("")
-                if generation_value[j] <= first_value:
-                    early_stop += 1
-                j -= 1
-        if early_stop >= early_stopping_patience:
-            print("Early stopping")
-            break
+        if early_stopping_patience != -1:
+            early_stop = 0
+            if curr_generation > early_stopping_patience:
+                first_value = generation_value[-1 - early_stopping_patience]
+                print(f"Checking early stopping: {first_value}", end=" ")
+                j = -1
+                while j > -early_stopping_patience - 1:
+                    print(f"{generation_value[j]}", end=" ")
+                    if j == -early_stopping_patience:
+                        print("")
+                    if generation_value[j] <= first_value:
+                        early_stop += 1
+                    j -= 1
+            if early_stop >= early_stopping_patience:
+                print("Early stopping")
+                break
         curr_generation += 1
     print(f"\nBest knapsack: {best_knapsack}, Value: {value(best_knapsack)}, Weight: {weight(best_knapsack)}")
 
@@ -118,7 +132,7 @@ def pbil():
 if __name__ == '__main__':
     pbil()
 
-# TODO parametry do pliku i problem
+# TODO parametry do pliku
 # TODO wykresy ----- jak zbierac dane
 # TODO sprawdzanie czasu wykonywania algorytmu
 # TODO przeniesienie do osobnego pliku
