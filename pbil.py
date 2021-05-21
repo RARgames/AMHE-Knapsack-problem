018d6b0282387e30c5930d4a0e47a67d9d7217cf
RAR <sswwqqaa@gmail.com>
2021-05-21 17:05:30 +0200

    fixed bugs

--- pbil.py
+++ pbil.py
@@ -1,13 +1,13 @@
 import random
 
-population_size = 2 #TODO 5
+population_size = 20
 max_weight = 26
-# weights = [12, 7, 11, 8, 9]
-# prices = [24, 13, 23, 15, 16]
 items = [(12, 24), (7, 13), (11, 23), (8, 15), (9, 16)]
-# TODO get from the file
 n = len(items)  # number of all items
 best_knapsack = [0] * n  # create array filled with zeros of length n, best knapsack so far
+generations = 20  # total amount of iterations
+mutation_probability = 0.2  # from 0 to 1
+mutation_value = 0.1  # from 0 to 1
 
 
 def value(item_set):
@@ -26,80 +26,70 @@ def weight(item_set):
     return curr_weight
 
 
-def create_population(v):
-    print("create")
-    p = [[0]*n for i in range(population_size)]  # empty 2d array
-    j = random.randint(0, n)
-    j += 1
+def create_population(probability_vec):
+    print("create_population")
+    p = [[0] * n for _ in range(population_size)]  # empty 2d array
     for i in range(1, population_size):
-        j = 0
-        while j < n:
+        for j in range(0, n):
             r = random.uniform(0, 1)
-            if v[j] > r:
+            if probability_vec[j] > r:
                 p[i][j] = 1
-            if weight(p[i]) > max_weight:
-                j = 0
-            else:
-                j += 1
-    # print("population {}", p)
     return p
 
 
-def get_best_knapsack(p):
-    print("get")
+def get_best_knapsack(population):
+    print("get_best_knapsack")
     max_value = 0
-    max_value_index = 0
+    max_value_index = -1
     for i in range(1, population_size):
-        curr_value = value(p[i])
-        if curr_value > max_value:
-            max_value = curr_value
-            max_value_index = i
-    return p[max_value_index]
+        if weight(population[i]) <= max_weight:
+            curr_value = value(population[i])
+            if curr_value > max_value:
+                max_value = curr_value
+                max_value_index = i
+    if max_value_index == -1:  # in case where there is no good candidate - try to fix it
+        return [0] * n  # empty array of length n
+    return population[max_value_index]
 
 
-def update_prob_vector(knapsack, v):
-    print("update")
-    alpha1 = 0.1  # learning rate 1
-    alpha2 = 0.2  # learning rate 2 - greater than alpha1
+def update_prob_vector(knapsack, probability_vec):
+    print("update_prob_vector")
+    learning_rate1 = 0.1
+    learning_rate2 = 0.2  # greater than learning_rate1
     global best_knapsack
+    # print("x ", best_knapsack)
+    # print("xd ", knapsack)
     if value(best_knapsack) >= value(knapsack):
-        alpha = alpha1
+        alpha = learning_rate1
     else:
-        alpha = alpha2
+        alpha = learning_rate2
         best_knapsack = knapsack
     for i in range(1, n):
-        v[i] = v[i] * (1 - alpha) + alpha * best_knapsack[i]
-    return v
+        probability_vec[i] = probability_vec[i] * (1 - alpha) + alpha * best_knapsack[i]
+    return probability_vec
 
 
-def mutate(v):
-    print("v")
-    m_v = 0.1  # mutation value #TODO to properties
-    m_p = 0.2  # mutation probability #TODO to properties
+def mutate(probability_vec):
+    print("mutate")
     for i in range(1, n):
-        r1 = random.uniform(0, 1)
         r2 = random.uniform(0, 1)
-        if m_p > r1:
-            v[i] = v[i] * (1 - m_v)
-            if r2 > 0.5 and v[i] + m_v <= 1:
-                v[i] = v[i] + m_v
-    return v
+        if mutation_probability > random.uniform(0, 1):
+            probability_vec[i] = probability_vec[i] * (1 - mutation_value)
+            if r2 > 0.5 and probability_vec[i] + mutation_value <= 1:
+                probability_vec[i] = probability_vec[i] + mutation_value
+    return probability_vec
 
 
-def main():
+def pbil():
     knapsack = best_knapsack
-    # p = # create empty 2d array
-    v = [.5] * n  # probability vector
-    max_iter = 5  # total amount of iterations # TODO increase to 100
-
-    for i in range(1, max_iter):
-        p = create_population(v)
-        knapsack = get_best_knapsack(p)
-        v = update_prob_vector(knapsack, v)
-        v = mutate(v)
+    probability_vec = [.5] * n  # initial probability vector
+    for i in range(1, generations):
+        population = create_population(probability_vec)
+        knapsack = get_best_knapsack(population)
+        probability_vec = update_prob_vector(knapsack, probability_vec)
+        probability_vec = mutate(probability_vec)
     print("Best knapsack: {}, Value: {}, Weight: {}".format(knapsack, value(knapsack), weight(knapsack)))
-    #return knapsack
 
 
 if __name__ == '__main__':
-    main()
+    pbil()
