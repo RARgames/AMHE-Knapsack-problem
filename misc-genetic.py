798852eb91eea516290269c4ec829fe6889a2c76
vlad <lagoikin@gmail.com>
2021-06-07 12:49:33 +0200

    genetic boxplot

--- misc-genetic.py
+++ misc-genetic.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import sys
+import numpy as np
+
 
 df = pd.read_csv("log_avgFit07_06_2021_12_37_05.csv")
 plt.plot(df['generation_counter'], df.iloc[:, 1], "r-", label="fitness")
