91aa2ecb3af59bce5ebc88b8a27ccb5aed813bb6
vlad <lagoikin@gmail.com>
2021-06-07 12:46:56 +0200

    genetic misc

---
+++ misc-genetic.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+df = pd.read_csv("log_avgFit07_06_2021_12_37_05.csv")
+plt.plot(df['generation_counter'], df.iloc[:, 1], "r-", label="fitness")
+plt.plot(df['generation_counter'], df.iloc[:, 2], "g-", label="best_fitness")
+plt.legend(loc="best")
+plt.ylabel('value')
+plt.xlabel('generation')
+plt.show()
+
